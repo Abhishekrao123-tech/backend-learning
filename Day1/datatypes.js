fc50d25b291dea430fb827d3430b285ec872db67
Abhishek Rao <abhishekraoh410@gmail.com>
2026-07-14 19:12:23 +0530

    datatypes

---
+++ Day1/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //all in new versions
+
+let age = 19;
+let name = "Abhishek Rao";
+let isloggedIn = false;
+let state = null;
+let accNo;
+// let num = 100 ^ 22255886289654896544896248;
+
+/*
+primitive data types
+number => 2^53
+string =>""
+bigint
+boolean=>true/false
+null=>standalone value its type is object
+undefines=> value not assigned
+symbol=> unique
+
+object
+
+*/
+console.log(name);
+console.log(typeof "abhi"); //string
+console.log(typeof age); //number
+console.log(typeof state); //object
+console.log(typeof accNo); //undefined
+console.log(typeof num); //number
+console.log(typeof null);

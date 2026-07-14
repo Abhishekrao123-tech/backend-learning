8a7e91784bbfc461be988752f0fbddf96cef2321
Abhishek Rao <abhishekraoh410@gmail.com>
2026-07-14 15:15:13 +0530

    variable ,constant,let and other basics

---
+++ Day1/variables.js
@@ -0,0 +1,18 @@
+console.log("Abhishek Rao");
+
+const accNo = 12254;
+
+let accEmail = "abc@google.com";
+var accPassword = "12345";
+accCity = "Yadgir";
+let accName;
+// accNo = 121212;   can't be changed
+
+/*
+not to use var 
+because of issue in scope / functional scope
+*/
+
+console.log(accNo);
+
+console.table([accNo, accEmail, accPassword, accCity, accName]);

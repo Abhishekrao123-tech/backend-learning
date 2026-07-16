c2e9bf057b0e73092dd9ed59856859516c7d865d
Abhishek Rao <abhishekraoh410@gmail.com>
2026-07-16 11:36:39 +0530

    operators in javascript

---
+++ Day2/operators.js
@@ -0,0 +1,32 @@
+// console.log(1 + 2);
+// console.log(1 - 2);
+// console.log(1 * 2);
+// console.log(1 ** 2); // 1^2
+// console.log(1 / 2);
+// console.log(1 % 2);
+
+let str1 = "Abhi";
+let str2 = "shek";
+
+let str = str1 + str2;
+// // console.log(str);
+
+//NOT A GOOD PRACTICE
+// console.log(1 + "2");
+// console.log(1 + 3 + "2");
+// console.log("2" + 1 + 3);
+/*
+if first operand is number then rest is converted into number
+but if the first operand is String then rest is converted into string
+*/
+
+// Incrementer operator
+let score = 100;
+// score++;
+// console.log(++score);
+// console.log(score++);
+// console.log(score);
+
+console.log(+true); //1
+console.log(+false); //0
+console.log(+""); //0

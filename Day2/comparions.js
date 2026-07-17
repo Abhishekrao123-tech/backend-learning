3bea3948170ec941586f89a1290bb2bf05a40e5d
Abhishek Rao <abhishekraoh410@gmail.com>
2026-07-17 11:44:56 +0530

    Comparions ,equality check and double equality check

---
+++ Day2/comparions.js
@@ -0,0 +1,24 @@
+console.log(2 < 1);
+console.log(2 >= 1);
+console.log(2 != 1);
+console.log(2 !== 1);
+console.log(2 == 1);
+
+//== converts the datatype of the operands to check for equality
+console.log("21" == 21);
+console.log(null == 0);
+
+console.log("2" < 1);
+
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);
+
+// console.log(undefined > 0);
+// console.log(undefined >= 0);
+// console.log(undefined == 0);
+
+//===   checks even datatype
+console.log("2" === 2);
+console.log(2 === 2);
+console.log("2" === "2");

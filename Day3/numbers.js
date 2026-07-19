66400ac1580729a1e89332e49e1132b197402e51
Abhishek Rao <abhishekraoh410@gmail.com>
2026-07-19 13:49:07 +0530

    Numbers in Js

---
+++ Day3/numbers.js
@@ -0,0 +1,11 @@
+let value = 400;
+
+const balance = new Number(100);
+const val = 200.223;
+console.log(value);
+console.log(balance);
+console.log(val.toFixed(1));
+console.log(val.toString().length);
+
+const num = 213.06556445;
+console.log(num.toPrecision(5));
